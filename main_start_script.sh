1bc275f018514d4e1442ee278729cb7790acb77a
Jatin <jatinnaik.9821@gmail.com>
2023-03-28 07:01:10 +0000

    Resolve temporary permission issues due to bind mount

--- main_start_script.sh
+++ main_start_script.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/sh
 
+# Temporary commands. Remove this, if the bind volume is changed/removed.
+chmod -R 777 /app/exchange-rate-app/var/cache
+chmod -R 777 /app/exchange-rate-app/var/log
+
+# Start the services
 service php8.2-fpm start
 service nginx start
